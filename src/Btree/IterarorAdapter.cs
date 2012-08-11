9a2abbbefa54b5f40c10ad27f02f804794b48dbc
jacek <none@none.com>
2012-08-11 07:29:20 +0000

    Initial version based on cleaned and updated NeoDatis.Net 1.9.6.0 beta

---
+++ src/Btree/IterarorAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace NDatabase.Btree
+{
+    public abstract class IterarorAdapter : IEnumerator
+    {
+        #region IEnumerator Members
+
+        public object Current
+        {
+            get { return GetCurrent(); }
+        }
+
+        public abstract bool MoveNext();
+        public abstract void Reset();
+
+        #endregion
+
+        public abstract object GetCurrent();
+    }
+}
