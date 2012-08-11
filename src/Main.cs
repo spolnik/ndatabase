eadbbd36d283892db5ec28310d7e71b66bb33b34
jacek <none@none.com>
2012-08-11 07:29:20 +0000

    Initial version based on cleaned and updated NeoDatis.Net 1.9.6.0 beta

---
+++ src/Main.cs
@@ -0,0 +1,17 @@
+// Main.cs created with MonoDevelop
+// User: olivier at 19:07 12/26/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+using System;
+
+namespace srccsharpnative
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
