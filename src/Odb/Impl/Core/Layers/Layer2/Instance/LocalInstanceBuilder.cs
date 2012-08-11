b1d5e4bb339be70e1e32e739d681cae5687e1c8b
jacek <none@none.com>
2012-08-11 07:29:20 +0000

    Initial version based on cleaned and updated NeoDatis.Net 1.9.6.0 beta

---
+++ src/Odb/Impl/Core/Layers/Layer2/Instance/LocalInstanceBuilder.cs
@@ -0,0 +1,20 @@
+using NDatabase.Odb.Core.Layers.Layer3;
+using NDatabase.Odb.Core.Transaction;
+
+namespace NDatabase.Odb.Impl.Core.Layers.Layer2.Instance
+{
+    public class LocalInstanceBuilder : InstanceBuilder
+    {
+        private readonly ISession _session;
+
+        public LocalInstanceBuilder(IStorageEngine engine) : base(engine)
+        {
+            _session = engine.GetSession(true);
+        }
+
+        protected override ISession GetSession()
+        {
+            return _session;
+        }
+    }
+}
---
+++ src/Odb/Impl/Core/Layers/Layer2/Instance/ODBClassPool.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NDatabase.Odb.Core;
+using NDatabase.Odb.Core.Layers.Layer2.Instance;
+using NDatabase.Tool.Wrappers.Map;
+
+namespace NDatabase.Odb.Impl.Core.Layers.Layer2.Instance
+{
+    /// <summary>
+    ///   A simple class pool, to optimize instance creation
+    /// </summary>
+    /// <author>osmadja</author>
+    public class OdbClassPool : IClassPool
+    {
+        private static readonly IDictionary<string, Type> ClassMap = new OdbHashMap<string, Type>();
+
+        private static readonly IDictionary<string, ConstructorInfo> ConstrutorsMap =
+            new OdbHashMap<string, ConstructorInfo>();
+
+        #region IClassPool Members
+
+        public virtual void Reset()
+        {
+            ClassMap.Clear();
+            ConstrutorsMap.Clear();
+        }
+
+        public virtual Type GetClass(string className)
+        {
+            lock (this)
+            {
+                Type clazz;
+                ClassMap.TryGetValue(className, out clazz);
+                if (clazz == null)
+                {
+                    try
+                    {
+                        clazz = Type.GetType(className);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new OdbRuntimeException(NDatabaseError.ClassPoolCreateClass.AddParameter(className), e);
+                    }
+                    ClassMap[className] = clazz;
+                }
+                return clazz;
+            }
+        }
+
+        public virtual ConstructorInfo GetConstrutor(string className)
+        {
+            ConstructorInfo constructorInfo;
+            ConstrutorsMap.TryGetValue(className, out constructorInfo);
+            return constructorInfo;
+        }
+
+        public virtual void AddConstrutor(string className, ConstructorInfo constructor)
+        {
+            ConstrutorsMap[className] = constructor;
+        }
+
+        #endregion
+    }
+}
