ee7e0873d2d4046961ed6765b71369315f37480d
jacek <none@none.com>
2012-08-11 07:29:20 +0000

    Initial version based on cleaned and updated NeoDatis.Net 1.9.6.0 beta

---
+++ CSharpStorage.UnitTests/Serialization/Identification/When_we_are_using_db_class_oid.cs
@@ -0,0 +1,64 @@
+﻿using NDatabase.Odb;
+using NDatabase.Odb.Core.Layers.Layer2.Meta;
+using NDatabase.Odb.Impl.Core.Oid;
+using NUnit.Framework;
+
+namespace NDatabase.UnitTests.Serialization.Identification
+{
+    public class When_we_are_using_db_class_oid : InstanceSpecification<OdbClassOID>
+    {
+        private long _oid;
+        private long _returnValue;
+
+        protected override void Establish_context()
+        {
+            _oid = 12345678L;
+        }
+
+        protected override OdbClassOID Create_subject_under_test()
+        {
+            return new OdbClassOID(_oid);
+        }
+
+        protected override void Because()
+        {
+            _returnValue = SubjectUnderTest.ObjectId;
+        }
+
+        [Test]
+        public void It_should_contain_object_id_equal_to_inserted_value()
+        {
+            Assert.That(_returnValue, Is.EqualTo(_oid));
+        }
+
+        [Test]
+        public void It_should_return_class_oid_type()
+        {
+            Assert.That(SubjectUnderTest.GetType(), Is.EqualTo(OdbType.ClassOidId));
+        }
+
+        [Test]
+        public void It_should_return_class_oid_type_name()
+        {
+            Assert.That(SubjectUnderTest.GetTypeName(), Is.EqualTo(OIDTypes.TypeNameClassOid));
+        }
+
+        [Test]
+        public void It_should_return_correct_string_representation_of_state()
+        {
+            var oidStringRepresentation = SubjectUnderTest.OidToString();
+
+            Assert.That(oidStringRepresentation, Is.EqualTo("class-oid:12345678"));
+        }
+
+        [Test]
+        public void It_should_create_string_which_will_help_to_create_equal_second_instance()
+        {
+            var oidStringRepresentation = SubjectUnderTest.OidToString();
+            var secondInstance = OdbClassOID.OidFromString(oidStringRepresentation);
+
+            Assert.That(secondInstance, Is.EqualTo(SubjectUnderTest));
+            Assert.That(secondInstance.GetHashCode(), Is.EqualTo(SubjectUnderTest.GetHashCode()));
+        }
+    }
+}
---
+++ CSharpStorage.UnitTests/Utils/When_working_with_class_utils.cs
@@ -0,0 +1,73 @@
+﻿using NDatabase.Tool.Wrappers;
+using NUnit.Framework;
+
+public class WithoutNamespace
+{
+
+}
+
+namespace NDatabase.UnitTests.Utils
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class When_working_with_class_utils
+    {
+        [Test] 
+        public void It_should_return_short_class_name_for_any_custom_class()
+        {
+            var className = OdbClassUtil.GetClassName(typeof (Person).FullName);
+            Assert.That(className, Is.EqualTo("Person"));
+        }
+
+        [Test]
+        public void It_should_return_short_class_name_for_any_base_type()
+        {
+            var stringClassName = OdbClassUtil.GetClassName(typeof (string).FullName);
+            Assert.That(stringClassName, Is.EqualTo("String"));
+
+            var intStructName = OdbClassUtil.GetClassName(typeof(int).FullName);
+            Assert.That(intStructName, Is.EqualTo("Int32"));
+        }
+
+        [Test]
+        public void It_should_return_namespace_for_any_custom_class()
+        {
+            var @namespace = OdbClassUtil.GetNamespace(typeof(Person).FullName);
+            Assert.That(@namespace, Is.EqualTo("NDatabase.UnitTests.Utils"));
+        }
+
+        [Test]
+        public void It_should_return_System_as_the_namespace_for_basic_types()
+        {
+            var @namespace = OdbClassUtil.GetNamespace(typeof(int).FullName);
+            Assert.That(@namespace, Is.EqualTo("System"));
+        }
+
+        [Test]
+        public void It_should_handle_properly_class_without_namespace()
+        {
+            var @namespace = OdbClassUtil.GetNamespace(typeof(WithoutNamespace).FullName);
+            Assert.That(@namespace, Is.Empty);
+
+            var className = OdbClassUtil.GetClassName(typeof(WithoutNamespace).FullName);
+            Assert.That(className, Is.EqualTo("WithoutNamespace"));
+        }
+
+        [Test]
+        public void It_should_return_proper_full_name_for_custom_class()
+        {
+            var fullName = OdbClassUtil.GetFullName(typeof (Person));
+            Assert.That(fullName, Is.EqualTo("NDatabase.UnitTests.Utils.Person,NDatabase.UnitTests"));
+        }
+
+        [Test]
+        public void It_should_return_proper_full_name_for_basic_type()
+        {
+            var fullName = OdbClassUtil.GetFullName(typeof(int));
+            Assert.That(fullName, Is.EqualTo("System.Int32,mscorlib"));
+        }
+    }
+}
