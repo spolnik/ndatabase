60ac017b3883b984a0a0df2db89c61a4a000011e
jacek <none@none.com>
2012-08-11 07:29:20 +0000

    Initial version based on cleaned and updated NeoDatis.Net 1.9.6.0 beta

---
+++ test/Function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Function
+    {
+        private string name;
+
+        public Function(string name)
+        {
+            this.name = name;
+        }
+        public string ToString()
+        {
+            return name;
+        }
+    }
+}
