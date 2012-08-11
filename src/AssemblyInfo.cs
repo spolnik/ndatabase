e9bd9fb10c4bab0de662b1befcc487542ffef57d
jacek <none@none.com>
2012-08-11 07:29:20 +0000

    Initial version based on cleaned and updated NeoDatis.Net 1.9.6.0 beta

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+// AssemblyInfo.cs created with MonoDevelop
+// User: olivier at 19:07 12/26/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("src-csharp-native")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NeoDatis")]
+[assembly: AssemblyProduct("NeoDatis ODB")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// If the build and revision are set to '*' they will be updated automatically.
+
+[assembly: AssemblyVersion("1.9.b.6")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
