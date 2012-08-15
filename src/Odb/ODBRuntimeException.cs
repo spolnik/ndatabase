c905939b2f035c5aef3e1cb176fecad468367fce
jacek <none@none.com>
2012-08-15 19:36:16 +0000

    Remove annoying error message Remove unnecessary Release class

--- src/Odb/ODBRuntimeException.cs
+++ src/Odb/ODBRuntimeException.cs
@@ -10,43 +10,33 @@ namespace NDatabase.Odb
     [Serializable]
     public class OdbRuntimeException : Exception
     {
-        private const string Url = "http://ndatabase.codeplex.com/workitem/list/basic";
-
         private static readonly string Message1 =
-            string.Format(
-                "\nNDatabase has thrown an Exception, please help us filling a bug report at {0} with the following error message",
-                Url);
+            string.Format("\nNDatabase has thrown an Exception");
 
         public OdbRuntimeException(IError error, Exception t)
             : base(
-                string.Format("{0}\nVersion={1} , Build={2}, Date={3}, Thread={4}\nError:{5}", Message1,
-                              Release.ReleaseNumber, Release.ReleaseBuild, Release.ReleaseDate,
+                string.Format("{0}\n Thread={1}\nError:{2}", Message1,
                               Thread.CurrentThread.Name, error), t)
         {
         }
 
         public OdbRuntimeException(IError error)
             : base(
-                string.Format("{0}\nVersion={1} , Build={2}, Date={3}, Thread={4}\nError:{5}", Message1,
-                              Release.ReleaseNumber, Release.ReleaseBuild, Release.ReleaseDate,
+                string.Format("{0}\nThread={1}\nError:{2}", Message1,
                               Thread.CurrentThread.Name, error))
         {
         }
 
         public OdbRuntimeException(IError error, string message)
             : base(
-                string.Format("{0}\nVersion={1} , Build={2}, Date={3}, Thread={4}\nError:{5}\nStackTrace:{6}", Message1,
-                              Release.ReleaseNumber, Release.ReleaseBuild, Release.ReleaseDate,
+                string.Format("{0}\nThread={1}\nError:{2}\nStackTrace:{3}", Message1,
                               Thread.CurrentThread.Name, error, message))
         {
         }
 
-        // FIXME add a submit a bug link to SF
-
         public OdbRuntimeException(Exception e, string message)
             : base(
-                string.Format("{0}\nVersion={1} , Build={2}, Date={3}, Thread={4}\nStackTrace:{5}", Message1,
-                              Release.ReleaseNumber, Release.ReleaseBuild, Release.ReleaseDate,
+                string.Format("{0}\nThread={1}\nStackTrace:{2}", Message1,
                               Thread.CurrentThread.Name, message), e)
         {
         }
