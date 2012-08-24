64643c4fe623be609ac7e2f07d89f1053115168e
jacek <none@none.com>
2012-08-24 16:31:15 +0000

    Update unit test

---
+++ test/NDatabase/Odb/Test/Jdk15/Generics/TestGenerics.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+
+namespace Test.Odb.Test.Jdk15.Generics
+{
+    /// <author>olivier</author>
+    [TestFixture]
+    public class TestGenerics : ODBTest
+    {
+        [Test]
+        public virtual void TestGetObects()
+        {
+            var baseName = GetBaseName();
+            var odb = Open(baseName);
+            odb.Store(new VO.Login.Function("Test"));
+            var functions = odb.GetObjects<VO.Login.Function>();
+            var f = functions.GetFirst();
+            odb.Close();
+            AssertEquals(1, functions.Count);
+        }
+
+        [Test]
+        public virtual void TestGetObects2()
+        {
+            var baseName = GetBaseName();
+            var odb = Open(baseName);
+            odb.Store(new VO.Login.Function("Test"));
+            var functions = odb.GetObjects<VO.Login.Function>();
+            var f = functions.Next();
+            odb.Close();
+            AssertEquals(1, functions.Count);
+        }
+
+        [Test]
+        public virtual void TestGetObects3()
+        {
+            var baseName = GetBaseName();
+            var odb = Open(baseName);
+            odb.Store(new VO.Login.Function("Test"));
+            var functions = odb.GetObjects<VO.Login.Function>();
+            var iterator = functions.GetEnumerator();
+            var f = iterator.Current;
+            odb.Close();
+            AssertEquals(1, functions.Count);
+        }
+    }
+}
---
+++ test/NDatabase/Odb/Test/List/TestLong.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+
+namespace Test.Odb.Test.List
+{
+    [TestFixture]
+    public class TestLong : NDatabaseAssert
+    {
+    }
+}
