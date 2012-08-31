a9014537e7e9762f8b82144d61c318e09ed8143f
jacek <none@none.com>
2012-08-31 19:49:25 +0000

    - add additional tests for MultiBuffer class

--- CSharpStorage.UnitTests/Layer3/When_we_use_multi_buffer_vo.cs
+++ CSharpStorage.UnitTests/Layer3/When_we_use_multi_buffer_vo.cs
@@ -33,20 +33,20 @@ namespace NDatabase.UnitTests.Layer3
             _buffer1EndPosition = 10L;
             _creationDate1 = OdbTime.GetCurrentTimeInTicks() + 1;
 
-            _buffer2StartPosition = 2L;
-            _buffer2EndPosition = 20L;
+            _buffer2StartPosition = 20L;
+            _buffer2EndPosition = 200L;
             _creationDate2 = OdbTime.GetCurrentTimeInTicks() + 2;
 
-            _buffer3StartPosition = 3L;
-            _buffer3EndPosition = 30L;
+            _buffer3StartPosition = 3000L;
+            _buffer3EndPosition = 3400L;
             _creationDate3 = OdbTime.GetCurrentTimeInTicks() + 3;
 
-            _buffer4StartPosition = 4L;
-            _buffer4EndPosition = 40L;
+            _buffer4StartPosition = 4000L;
+            _buffer4EndPosition = 4300L;
             _creationDate4 = OdbTime.GetCurrentTimeInTicks() + 4;
 
-            _buffer5StartPosition = 5L;
-            _buffer5EndPosition = 50L;
+            _buffer5StartPosition = 5000L;
+            _buffer5EndPosition = 5400L;
             _creationDate5 = OdbTime.GetCurrentTimeInTicks() + 5;
         }
 
@@ -138,6 +138,61 @@ namespace NDatabase.UnitTests.Layer3
             Assert.That(SubjectUnderTest.Buffers[bufferIndex][possitionInBuffer], Is.EqualTo(value));
 
             Assert.That(SubjectUnderTest.MaxPositionInBuffer[bufferIndex], Is.EqualTo(possitionInBuffer));
+        }
+
+        [Test]
+        public void It_should_return_correct_position_when_checking_the_buffor()
+        {
+            var bufferIndex1 = SubjectUnderTest.GetBufferIndexForPosition(_buffer1StartPosition, (int) (_buffer1EndPosition - _buffer1StartPosition));
+            Assert.That(bufferIndex1, Is.EqualTo(0));
+
+
+            var bufferIndex3 = SubjectUnderTest.GetBufferIndexForPosition(_buffer3StartPosition, (int)(_buffer3EndPosition - _buffer3StartPosition));
+            Assert.That(bufferIndex3, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void It_should_return_minus_1_when_requested_position_is_out_of_range()
+        {
+            const int startOutOfRange = 2000;
+
+            var bufferIndex = SubjectUnderTest.GetBufferIndexForPosition(startOutOfRange, 400);
+            Assert.That(bufferIndex, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void It_should_have_empty_buffer_after_cleaning_it()
+        {
+            const byte value = 2;
+            const int bufferIndex = 3;
+            const int possitionInBuffer = 1;
+
+            SubjectUnderTest.SetByte(bufferIndex, possitionInBuffer, value);
+            Assert.That(SubjectUnderTest.Buffers[bufferIndex][possitionInBuffer], Is.EqualTo(value));
+
+            var maxPosition = SubjectUnderTest.MaxPositionInBuffer[bufferIndex];
+            SubjectUnderTest.ClearBuffer(bufferIndex);
+
+            for (var i = 0; i < maxPosition; i++)
+                Assert.That(SubjectUnderTest.Buffers[bufferIndex][i], Is.EqualTo(0));
+
+            Assert.That(SubjectUnderTest.MaxPositionInBuffer[bufferIndex], Is.EqualTo(0));
+            Assert.That(SubjectUnderTest.BufferPositions[bufferIndex].Start, Is.EqualTo(0));
+            Assert.That(SubjectUnderTest.BufferPositions[bufferIndex].End, Is.EqualTo(0));
+            Assert.That(SubjectUnderTest.HasBeenUsedForWrite(bufferIndex), Is.False);
+        }
+
+        [Test]
+        public void It_should_properly_write_array_of_bytes()
+        {
+            const int bufferIndex = 1;
+            byte[] bytes = {1, 2, 3};
+
+            SubjectUnderTest.WriteBytes(bufferIndex, bytes, 0, 10, bytes.Length);
+
+            Assert.That(SubjectUnderTest.Buffers[bufferIndex][10], Is.EqualTo(1));
+            Assert.That(SubjectUnderTest.Buffers[bufferIndex][10 + 1], Is.EqualTo(2));
+            Assert.That(SubjectUnderTest.Buffers[bufferIndex][10 + 2], Is.EqualTo(3));
         }
     }
 }
