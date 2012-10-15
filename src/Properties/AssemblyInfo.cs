3880f5677244670d1b434850cae63f1f930d8b3d
jacek <none@none.com>
2012-10-15 18:56:33 +0000

    - Apply version change

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -38,8 +38,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta")]
+[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyInformationalVersion("2.0.1-stable")]
 [assembly: InternalsVisibleTo("NDatabase.UnitTests")]
 [assembly: InternalsVisibleTo("NDatabase.Old.UnitTests")]
