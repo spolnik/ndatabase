65718b44f39a9243d2a7d9e7f88070dc787150ba
jacek <jacek.spolnik@gmail.com>
2012-12-23 16:50:40 +0100

    Remove duplication in RefactorManager

--- src/Odb/Core/Layers/Layer3/Refactor/RefactorManager.cs
+++ src/Odb/Core/Layers/Layer3/Refactor/RefactorManager.cs
@@ -4,56 +4,54 @@ using NDatabase2.Odb.Core.Layers.Layer2.Meta;
 
 namespace NDatabase2.Odb.Core.Layers.Layer3.Refactor
 {
-    //TODO: check if auto ordering of fields didn't break the refactor manager 
     public sealed class RefactorManager : IRefactorManager
     {
-        private readonly IStorageEngine _storageEngine;
+        private readonly MetaModel _metaModel;
+        private readonly IObjectWriter _objectWriter;
 
         internal RefactorManager(IStorageEngine storageEngine)
         {
-            _storageEngine = storageEngine;
+            _metaModel = storageEngine.GetSession(true).GetMetaModel();
+            _objectWriter = storageEngine.GetObjectWriter();
         }
 
         #region IRefactorManager Members
 
         public void AddField(Type type, Type fieldType, string fieldName)
         {
-            var metaModel = _storageEngine.GetSession(true).GetMetaModel();
-            var ci = metaModel.GetClassInfo(type, true);
+            var ci = _metaModel.GetClassInfo(type, true);
             // The real attribute id (-1) will be set in the ci.addAttribute
             var cai = new ClassAttributeInfo(-1, fieldName, fieldType.FullName, ci);
             ci.AddAttribute(cai);
-            _storageEngine.GetObjectWriter().UpdateClassInfo(ci, true);
+            _objectWriter.UpdateClassInfo(ci, true);
         }
 
         public void RemoveField(Type type, string attributeName)
         {
-            var metaModel = _storageEngine.GetSession(true).GetMetaModel();
-            var ci = metaModel.GetClassInfo(type, true);
+            var ci = _metaModel.GetClassInfo(type, true);
             var cai2 = ci.GetAttributeInfoFromName(attributeName);
             ci.RemoveAttribute(cai2);
-            _storageEngine.GetObjectWriter().UpdateClassInfo(ci, true);
+            _objectWriter.UpdateClassInfo(ci, true);
         }
 
         public void RenameClass(string fullClassName, string newFullClassName)
         {
-            var metaModel = _storageEngine.GetSession(true).GetMetaModel();
-            var ci = metaModel.GetClassInfo(fullClassName, true);
+            var ci = _metaModel.GetClassInfo(fullClassName, true);
 
-            var setFullClassName = ci.GetType().GetProperty("FullClassName", BindingFlags.SetField | BindingFlags.NonPublic);
+            var setFullClassName = ci.GetType().GetProperty("FullClassName",
+                                                            BindingFlags.SetField | BindingFlags.NonPublic);
             var setMethod = setFullClassName.GetSetMethod(true);
             setMethod.Invoke(ci, new object[] {newFullClassName});
-            
-            _storageEngine.GetObjectWriter().UpdateClassInfo(ci, true);
+
+            _objectWriter.UpdateClassInfo(ci, true);
         }
 
         public void RenameField(Type type, string attributeName, string newAttributeName)
         {
-            var metaModel = _storageEngine.GetSession(true).GetMetaModel();
-            var ci = metaModel.GetClassInfo(type, true);
+            var ci = _metaModel.GetClassInfo(type, true);
             var cai2 = ci.GetAttributeInfoFromName(attributeName);
             cai2.SetName(newAttributeName);
-            _storageEngine.GetObjectWriter().UpdateClassInfo(ci, true);
+            _objectWriter.UpdateClassInfo(ci, true);
         }
 
         #endregion
