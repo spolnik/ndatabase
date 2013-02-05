0f62e8a4197324b397aedc4e997b57e02b892674
jacek <jacek.spolnik@gmail.com>
2013-02-05 22:19:38 +0100

    add 3.4 version of Northwind db update performance tester

--- tools/NDatabase.Performance.Tester/Program.cs
+++ tools/NDatabase.Performance.Tester/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using NDatabase.Odb;
 using NDatabase.Performance.Tester.Domain;
+using System.Linq;
 
 namespace NDatabase.Performance.Tester
 {
@@ -11,10 +12,16 @@ namespace NDatabase.Performance.Tester
 
         static void Main(string[] args)
         {
+            if (args[0] == "q")
+            {
+                CheckQuery();
+                return;
+            }
+
             var count = Convert.ToInt32(args[0]);
 
             OdbFactory.Delete(DbName);
-            
+
             for (var i = 1; i <= count; i++)
             {
                 Console.WriteLine("Start processing: " + i + ".");
@@ -28,36 +35,54 @@ namespace NDatabase.Performance.Tester
 
                 Console.WriteLine("Processed " + i * (100 * 100 * 100 + 1) + " items in " + stopwatch.ElapsedMilliseconds + " ms.");
 
-//                Console.WriteLine("Start generating count for " + i + " iteration.");
-//                stopwatch.Reset();
-//                stopwatch.Start();
-//                long count1;
-//                using (var odb = OdbFactory.Open(DbName))
-//                {
-//                    count1 = odb.QueryAndExecute<object>().Count;
-//                }
-//                stopwatch.Stop();
-//                Console.WriteLine("Items in db: " + count1 + ". Time: " + stopwatch.ElapsedMilliseconds + " ms.");
+                //                Console.WriteLine("Start generating count for " + i + " iteration.");
+                //                stopwatch.Reset();
+                //                stopwatch.Start();
+                //                long count1;
+                //                using (var odb = OdbFactory.Open(DbName))
+                //                {
+                //                    count1 = odb.QueryAndExecute<object>().Count;
+                //                }
+                //                stopwatch.Stop();
+                //                Console.WriteLine("Items in db: " + count1 + ". Time: " + stopwatch.ElapsedMilliseconds + " ms.");
             }
+        }
+
+        private static void CheckQuery()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            using (var odb = OdbFactory.Open(DbName))
+            {
+                var storedItem = (from item in odb.QueryAndExecute<HolderB>().AsQueryable()
+                                 where item.Id.Equals(10)
+                                 select item).First();
+
+                Console.WriteLine(storedItem.Name);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed: " + stopwatch.ElapsedMilliseconds + "ms.");
         }
 
         private static object PrepareNewItem(int i)
         {
-            var holderA = new HolderA {Id = i, Name = "HolderA" + i};
+            var holderA = new HolderA { Id = i, Name = "HolderA" + i };
 
             for (var j = 0; j < 100; j++)
             {
-                var holderB = new HolderB {Id = j, Name = "HolderB" + j};
+                var holderB = new HolderB { Id = j, Name = "HolderB" + j };
                 holderA.ListOfHolderBItems.Add(holderB);
 
                 for (var k = 0; k < 100; k++)
                 {
-                    var holderC = new HolderC {Id = k, Name = "HolderC" + k};
+                    var holderC = new HolderC { Id = k, Name = "HolderC" + k };
                     holderB.ListOfHolderCItems.Add(holderC);
 
                     for (var l = 0; l < 100; l++)
                     {
-                        var item = new FinalItem {Id = l, Name = "FinalItem" + l};
+                        var item = new FinalItem { Id = l, Name = "FinalItem" + l };
                         holderC.FinalItems.Add(item);
                     }
                 }
