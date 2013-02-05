8fcaad54b58e14fee69d6ba531cbf5db271fe974
jacek <jacek.spolnik@gmail.com>
2013-02-05 22:43:59 +0100

    Update project after merging from master

--- src/Properties/AssemblyInfo.CF.cs
+++ src/Properties/AssemblyInfo.CF.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.3.0.*")]
-[assembly: AssemblyInformationalVersion("3.3.0-stable")]
+[assembly: AssemblyVersion("3.4.0.*")]
+[assembly: AssemblyInformationalVersion("3.4.0-stable")]
 
 [assembly: InternalsVisibleTo("NDatabase.UnitTests")]
 [assembly: InternalsVisibleTo("NDatabase.Old.UnitTests")]
